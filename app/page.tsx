6828054bfe5f95f69b18274ea727c6c1d1a8aee5
Phalla <55537405+phalla-doll@users.noreply.github.com>
2026-05-08 09:47:11 +0700

    feat: Add curved background swoosh graphic
    Introduces a new decorative background element to enhance the visual appeal of the coffee display. This change adds a curved swoosh graphic behind the carousel, providing a more dynamic and engaging user interface.

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,15 @@ import { Carousel } from "@/components/carousel";
 export default function Home() {
   return (
     <main className="w-screen h-screen bg-white relative overflow-hidden flex flex-col">
-      {/* Background Decorative Graphic (Light Mint Circle) */}
-      <div className="absolute top-[-20%] left-[20%] w-[900px] h-[900px] bg-[var(--color-starbucks-light)] rounded-full -z-10"></div>
+      {/* Background Decorative Graphics */}
+      <div className="absolute top-[-20%] left-[20%] w-[900px] h-[900px] bg-[#E8F1EC] rounded-full -z-10"></div>
+      
+      {/* Curved background swoosh behind carousel */}
+      <div className="absolute bottom-0 left-0 w-full h-[60vh] -z-10 overflow-hidden">
+        <svg viewBox="0 0 1440 400" className="absolute bottom-0 w-full min-w-[1440px] h-full" preserveAspectRatio="none">
+          <path fill="#F4F4F0" d="M0,250 C300,350 700,50 1440,250 L1440,400 L0,400 Z"></path>
+        </svg>
+      </div>
 
       <div className="flex-1 w-full px-16 relative z-10 flex flex-col pt-16 mx-auto max-w-[1440px]">
         {/* Header Section */}
